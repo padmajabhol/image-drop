9b9f7245a588f52aac4f804e3880369cdf7e38c3
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2023-03-26 03:29:24 +0530

    router to create user

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from db import models
 from db.database import engine
+from routers import users
 
 app = FastAPI()
+
+models.Base.metadata.create_all(engine)
+
+app.include_router(users.router)
 
 @app.get("/")
 def root():
     return "Initial commit!"
 
-models.Base.metadata.create_all(engine)
