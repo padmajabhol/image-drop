a10da1e9caedf2fff98bab8376e9b34b52ff4cb8
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2023-03-26 14:26:00 +0530

    hash password

--- db/db_user.py
+++ db/db_user.py
@@ -1,13 +1,13 @@
 from routers.schemas import UserBase
 from sqlalchemy.orm.session import Session
 from .models import DbUser
+from .hasing import Hash
 
 def create(request: UserBase, db: Session):
     new_user = DbUser(
         username = request.username,
         email = request.email,
-        #TODO hash password
-        password = request.password
+        password = Hash.bcrypt(request.password)
     )
     db.add(new_user)
     db.commit()
--- routers/schemas.py
+++ routers/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class UserBase(BaseModel):
     username: str
@@ -8,3 +9,29 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
     username: str
     email: str
+    class Config():
+        orm_mode = True
+
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str
+    caption: str
+    creator_id: int
+
+# Post display
+
+class User(BaseModel):
+    username: str
+    class Config():
+        orm_mode = True
+
+class PostDisplay(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: datetime
+    user: User
+    class Config():
+        orm_mode = True
+
