6475a42f0d25a1f7f37175cb0292fe59c2ff72f3
Padmaja Bhol <75530516+padmajabhol@users.noreply.github.com>
2023-03-26 15:54:45 +0530

    router to create post

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 from db import models
 from db.database import engine
-from routers import users
+from routers import posts, users
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
 app.include_router(users.router)
+app.include_router(posts.router)
 
 @app.get("/")
 def root():
---
+++ routers/posts.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from .schemas import PostBase, PostDisplay
+from db.database import get_db
+from db.db_post import create
+
+router = APIRouter(prefix="/posts", tags=['post'])
+
+image_url_types = ['absolute', 'relative']
+
+@router.post("/", response_model=PostDisplay)
+def create_post(request: PostBase, db: Session = Depends(get_db)):
+    if not request.image_url_type in image_url_types:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            detail= "Parameter image_url_type can only take values 'absolute' and 'relative'.")
+    return create(request, db)
